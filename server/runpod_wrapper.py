4500a4feab59fe2996cfbf8cb688523bae038810
cgi <christian.gintenreiter@smarter-ecommerce.com>
2024-07-28 18:45:04 +0200

    add debug

--- server/runpod_wrapper.py
+++ server/runpod_wrapper.py
@@ -9,10 +9,17 @@ request_timeout = float(os.getenv("REQUEST_TIMEOUT", 300.0))
 limits = httpx.Limits(max_connections=concurrency_max, max_keepalive_connections=concurrency_max)
 client = httpx.AsyncClient(verify=False, limits=limits, timeout=request_timeout)
 
+debug = os.getenv("DEBUG", "false")
+
+def print_if_debug(message):
+    if debug != "false":
+        print(message)
+
 async def handler(job):
     try:
         base_url = "http://0.0.0.0:11434"
         input = job['input']
+        print_if_debug(f"Input: {input}")
         if 'url_path' not in input:
             raise Exception(f"Property url_path is required but not found in job-keys {input.keys()}")
         url_path = input['url_path']
@@ -25,9 +32,8 @@ async def handler(job):
         )
         response.encoding = "utf-8"
 
-        # TODO: handle errors
         result = response.json()
-        print("Result:", result)
+        print_if_debug(f"Result: {result}")
         return result
     except Exception as e:
         print("Error:", str(e))
